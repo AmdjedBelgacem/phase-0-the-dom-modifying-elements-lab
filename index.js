903e5d1c71b4a42d3a9c7481cac3cdb7dc84f206
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-04-22 17:33:19 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const removingmain = document.getElementById("main");
+removingmain.remove();
+
+const newHeader = document.createElement("h1")
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Amdjed is the champion";
